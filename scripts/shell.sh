9aab6851b53f6e937f2609f27ef4b01b5fe52965
Hugo Dutka <contact@hugodutka.com>
2020-10-28 12:59:45 +0100

    fix source env in shell.sh

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,4 +1,4 @@
 # import env variables from the .env file
-set -a && . ~/.hocus-mount/.env set +a && \
+set -a && . ~/.hocus-mount/.generated/.env set +a && \
 # run bash and pass over script arguments
 /bin/bash "$@"
